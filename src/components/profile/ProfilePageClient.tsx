8cafd0a74e07e70f25ac416d431136279b9aa70a
unknown <itingeocad@gmail.com>
2026-03-27 13:55:54 +0200

    AICar: force add ListingsSectionClient import in profile page

--- src/components/profile/ProfilePageClient.tsx
+++ src/components/profile/ProfilePageClient.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { ListingsSectionClient } from '@/components/profile/ListingsSectionClient';
+
 import Link from 'next/link';
 import { useEffect, useMemo, useState } from 'react';
 import { upload } from '@vercel/blob/client';
